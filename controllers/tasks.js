73b613adef2ead9f33a8dcaa989e4a68ca796411
Syed Maaz Hussaini <35537380+maazhussaini@users.noreply.github.com>
2021-10-17 23:13:09 +0500

    Rest API

---
+++ controllers/tasks.js
@@ -0,0 +1,25 @@
+const getAllTasks = (req, res) => {
+    res.send('Get all tasks')
+}
+
+const createTask = (req, res) => {
+    // res.status(200).json({ message: 'ok' })
+    
+    res.status(200).json(req.body)
+}
+
+const getTask = (req, res) => {
+    res.send({id:req.params.id})
+}
+
+const updateTask = (req, res) => {
+    res.send('Task updated')
+}
+
+const deleteTask = (req, res) => {
+    res.send('Task deleted')
+}
+
+module.exports = {
+    getAllTasks, createTask, getTask, updateTask, deleteTask
+}
